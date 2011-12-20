7e8bf9451d85a868a7c741d8ab059d4286285ca4
Jeen Lee <jeen@perl.kr>
2011-12-21 00:10:03 +0900

    modified something

--- lib/Bobby/BaseHandler.pm
+++ lib/Bobby/BaseHandler.pm
@@ -5,23 +5,8 @@ use namespace::autoclean;
 extends 'Tatsumaki::Handler';
 
 sub session {}
-=pod
-sub session {
-    my ($self,$k,$v) = @_;
+sub oauth_token {}
 
-    !$k and !$v and return undef;
-
-    my $session = Plack::Session->new($self->request->env);
-    return $session->get($k) unless $v;
-    $session->set($k, $v);
-}
-
-sub oauth_token {
-    my ($self, $service) = @_;
-
-    $self->session($service . "_token");
-}
-=cut
 __PACKAGE__->meta->make_immutable;
 
 1;
