80d9ca5f97431fdc81398334221044aa4e583c68
Jeen Lee <jeen@perl.kr>
2011-12-20 18:53:09 +0900

    Added Plack::Middleware::OAuth::Foursquare Added Plack::Middleware::OAuth::UserInfo::Foursquare

---
+++ lib/Plack/Middleware/OAuth/Foursquare.pm
@@ -0,0 +1,15 @@
+package Plack::Middleware::OAuth::Foursquare;
+use strict;
+use warnings;
+
+sub config {
+  +{
+    version       => 2,
+    authorize_url => 'https://foursquare.com/oauth2/authenticate',
+    access_token_url => 'https://foursquare.com/oauth2/access_token',
+    response_type    => 'code',
+    grant_type       => 'authorization_code',
+  };
+}
+
+1;
---
+++ lib/Plack/Middleware/OAuth/UserInfo/Foursquare.pm
@@ -0,0 +1,26 @@
+package Plack::Middleware::UserInfo::Foursquare;
+use strict;
+use warnings;
+use parent qw(Plack::Middleware::UserInfo);
+use LWP::UserAgent;
+use JSON;
+
+sub create_handle {
+    my $self = shift;
+
+}
+
+sub query {
+    my $self = shift;
+
+    my $uri = URI->new('https://api.foursquare.com/v2/users/self');
+    $uri->query_form( oauth_token => $self->token->access_token );
+    my $res = LWP::UserAgent->new->get($uri);
+    my $body = $res->decoded_content;
+    return unless $body;
+
+    my $obj = decode_json($body) || {};
+    return $obj->{response}->{user};
+}
+
+1;
